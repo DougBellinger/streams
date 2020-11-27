a317c19606fe0cac8aa8820d89711995db731f8b
DougBellinger <doug.bellinger@gmail.com>
2020-11-27 12:48:42 -0500

    cleaned up

--- streams.sh
+++ streams.sh
@@ -2,4 +2,4 @@
 clear
 HOST_ADDR=$(hostname -I)
 echo 'Running on '$HOST_ADDR
-sudo iftop -P -t -n | awk -v h=$HOST_ADDR -f addrs.awk
+sudo iftop -c iftop.cfg -P -t -n | awk -v h=$HOST_ADDR -f addrs.awk
